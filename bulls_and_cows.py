576b2622215ca062fb5c04269076138f00b4428e
kai.reytsu <kai.reytsu@gmail.com>
2022-02-09 17:32:11 +0300

    Fix errors. Add discriptions. Add a comparison if the numbers are of the wrong length

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -11,6 +11,7 @@ import tkinter.dialog as dg
 from random import randint
 
 def random_number():
+    '''Функция возвращающая случайное число из 4 цифр'''
     list_num = []
     step = 0
     list_len = 4
@@ -22,13 +23,23 @@ def random_number():
     else:
         return random_number()
 
+
 def game_play(computer_number):
+    '''Функция обработки вводимых пользователем чисел'''
     bulls = 0
     cows = 0
-    user_number = ent.get()             
+    user_number = ent.get() 
     user_input = str(user_number)[0:4]
     user_map = map(int, user_input)
-    user_list = list(user_map)  
+    user_list = list(user_map)
+    if len(computer_number) > len(user_list):
+        msg2.config(text = msg2.cget('text') + 'Ввели недастаточно цифр \n')
+        ent.delete(0, tk.END)
+        return None
+    if len(set(user_list)) != len(user_list):
+        msg2.config(text = msg2.cget('text') + 'Ввели одинаковые цифры \n')
+        ent.delete(0, tk.END)
+        return None
     if computer_number == user_list:
         return 'win!'
     for index in range(4):
@@ -39,13 +50,13 @@ def game_play(computer_number):
     msg2.config(text = msg2.cget('text') + 'Ваше число: ' + user_input + ' содержит ' + str(
                                 bulls) + ' быка и ' + str(
                                     cows) + ' коровы\n')
-    ent.delete(0, tk.END)                          
-    return game_play(computer_number)
+    ent.delete(0, tk.END)
 
-def play(event): 
+def play(event):
+    '''Функция запуска обработки вводимых чисел'''
     global number
-    game_play(number)
-    if game_play(number) == 'win!':
+    gameplay = game_play(number)
+    if gameplay == 'win!':
         gameOwer = dg.Dialog(title = 'Вы победили!',
                text = '     Сыграем ещё?           ',
                bitmap = 'questhead',
@@ -56,34 +67,41 @@ def play(event):
         else:
             number = random_number()
             print(number)
-            return play(event)
+            msg2.config(text='')
+            ent.delete(0, tk.END) 
+            
 
 
 number = random_number()
 print(number)  
 
-
+#Создание и настройка gui
 root = tk.Tk()
-root.geometry('500x350')
+root.geometry('500x700')
 root.title('Быки и Коровы')
 root.configure(bg='#5097ab')
+
+#Текст с правилами игры
 msg = tk.Message(width=400, padx=10, pady=5,
               text='Введите число из 4 цифр.'
-              'Цифры не должны повторяться,'
-              'Быки - угаданные цифры, находящиеся на нужном месте'
+              'Цифры не должны повторяться. \n'
+              'Быки - угаданные цифры, находящиеся на нужном месте. \n'
               'Коровы - угаданные цифры, находящиеся не на нужном месте')
 msg.pack(padx=10, pady=5)                      
 msg.config(bg='#5097ab', fg='#3d1515',
            font=('Arial', 12, 'bold', 'italic'))
 
-ent = tk.Entry(width=4)
+#Поле ввода чисел пользователем
+ent = tk.Entry(width=8)
 ent.pack()
 ent.focus()
 ent.bind( '<Return>', play)  
 
+#Вывод результатов сравнения вводимых пользователем чисел
 msg2 = tk.Message(width=400, padx=10, pady=5, text='')
 msg2.pack(padx=10, pady=5)                     
 msg2.config(bg='#5097ab',
         font=('times', 12, 'normal'))  
 
+#Запуск gui
 root.mainloop()
